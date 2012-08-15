627826aa9c63c89769466228fa0dd396cf1a1404
Samir Talwar <samir@noodlesandwich.com>
2012-08-15 23:53:21 +0100

    Removed unnecessary type information.

--- test/Hamcrest/MatchersTests.hs
+++ test/Hamcrest/MatchersTests.hs
@@ -5,7 +5,6 @@ import Test.QuickCheck (Property)
 import Hamcrest.Matchers
 import Hamcrest.Assertions (expectThat)
 
-equalToTests :: [Int -> Property]
 equalToTests =
     [\x -> expectThat x (equalTo x),
      \x -> expectThat x (not_ (equalTo (x + 1))),
