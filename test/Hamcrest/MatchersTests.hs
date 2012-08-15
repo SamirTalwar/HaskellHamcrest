cff9b6851bb708ee3f581effade34db5d628da27
Samir Talwar <samir@noodlesandwich.com>
2012-08-16 00:04:13 +0100

    not (x < x) and not (x > x).

--- test/Hamcrest/MatchersTests.hs
+++ test/Hamcrest/MatchersTests.hs
@@ -15,6 +15,7 @@ equalToTests =
 greaterThanTests =
     [\x -> expectThat x (greaterThan (x - 2)),
      \x -> expectThat x (not_ (greaterThan (x + 1))),
+     \x -> expectThat x (not_ (greaterThan x)),
 
      \x -> expectThat (greaterThan x) (describesItselfAs ("greater than " ++ show x)),
      \x -> expectThat (greaterThan x) (describesAMismatchFor (x - 1) (show $ x - 1))]
@@ -22,6 +23,7 @@ greaterThanTests =
 lessThanTests =
     [\x -> expectThat x (lessThan (x + 4)),
      \x -> expectThat x (not_ (lessThan (x - 1))),
+     \x -> expectThat x (not_ (lessThan x)),
 
      \x -> expectThat (lessThan x) (describesItselfAs ("less than " ++ show x)),
      \x -> expectThat (lessThan x) (describesAMismatchFor (x + 6) (show $ x + 6))]
